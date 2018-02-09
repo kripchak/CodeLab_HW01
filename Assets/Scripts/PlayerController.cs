d540eb59ac53f09ccd4de2237f6c61fce334ab01
Unknown <mlk463@nyu.edu>
2018-02-09 08:11:07 -0500

    Initial Commit
    First commit attempt

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private Rigidbody rb;
+	public float speed;
+	public float xMin, xMax, zMin, zMax;
+
+	// Use this for initialization
+	void Start () {
+
+		rb = GetComponent<Rigidbody>();
+
+		
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveHorizontal = Input.GetAxis ("Horizontal");
+		float moveVertical = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (moveHorizontal, 0.0f, moveVertical);
+
+		rb.velocity = movement*speed;
+		rb.position = new Vector3 (
+			Mathf.Clamp (rb.position.x, xMin, xMax),
+			0.0f,
+			Mathf.Clamp (rb.position.z, zMin, zMax)
+		);
+	}
+}
